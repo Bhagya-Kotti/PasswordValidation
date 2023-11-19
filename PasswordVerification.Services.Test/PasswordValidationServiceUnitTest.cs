28d47dcdb90559739ece7d4e7930cc4d1d2774fa
Bhagya-Kotti <bhaginavi@gmail.com>
2023-11-19 13:08:31 -0500

    Intial Test Scnenario added for Password Length

--- PasswordVerification.Services.Test/PasswordValidationServiceUnitTest.cs
+++ PasswordVerification.Services.Test/PasswordValidationServiceUnitTest.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
 using NUnit.Framework;
 using System;
 
 namespace PasswordValidation.Services.Test
 {
     [TestFixture]
-    public class UnitTest1
+    public class PasswordValidationServiceUnitTest
     {
+        //1.Lenght should be 8 charachers
+        //2.Atleast 2 numbers
         [Test]
-        public void TestMethod1()
+        public void ScenarioWherePasswordIsAtleastEightCharctersLong()
         {
+            int actualValue = 10;
+            int expectedValue = 8;
+            Assert.IsTrue(actualValue >= expectedValue);
         }
     }
 }
